ad4aadd549d87200fb3b4519f627d0bc14756523
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2014-05-15 22:29:09 +0200

    Replaces context with theories.

--- Source/When_calculating_a_generation.cs
+++ Source/When_calculating_a_generation.cs
@@ -7,26 +7,21 @@ using Xunit.Extensions;
 public class When_calculating_a_generation
 {
 	Fixture fixture;
-	Grid subject;
-	Generation seed;
-	Generation nextGeneration;
 
 	public When_calculating_a_generation()
 	{
 		fixture = new Fixture();
-		subject = fixture.Create<Grid>();
-		seed = fixture.Create<Generation>();
-		nextGeneration = subject.Refresh(seed);
 	}
 
-	[Fact]
-	public void It_should_return_a_generation_containing_cells()
+	[Theory, AutoData]
+	public void It_should_return_a_generation_containing_cells(Grid subject, Generation seed)
 	{
+	    var nextGeneration = subject.Refresh(seed);
 		nextGeneration.Should().NotBeEmpty();
 	}
 
 	[Theory, AutoData]
-	public void It_should_return_the_same_amount_of_cells(Generation seed)
+	public void It_should_return_the_same_amount_of_cells(Grid subject, Generation seed)
 	{
 	    var nextGeneration = subject.Refresh(seed);
 	    nextGeneration.Should().HaveSameCount(seed);
